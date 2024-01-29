6834a3a6a97636d1a807d3ac3be5fa17abf0cb6b
nizarfdlh <suncodev@gmail.com>
2024-01-29 19:50:07 +0700

    update user models

--- app/models/user_models.js
+++ app/models/user_models.js
@@ -1,25 +1,29 @@
 const sequelize = require('../database/db_config');
-const { DataTypes } = require('sequelize')
+const { DataTypes } = require('sequelize');
 
 const User = sequelize.define('users', {
     id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
-        autoIncrement: true,
     },
     name: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: true,
     },
     email: {
         type: DataTypes.STRING,
-        allowNull: false,
-        unique: true,
+        allowNull: true,
     },
     password: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: true,
     },
+    createdAt: {
+        type: DataTypes.TIME,
+    },
+    updatedAt: {
+        type: DataTypes.DATE,
+    }
 });
 
 module.exports = User;
