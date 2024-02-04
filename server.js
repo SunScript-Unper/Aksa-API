e291b29a429f2a38f9b70ef22562a1bb68899048
nizarfdlh <suncodev@gmail.com>
2024-02-04 19:05:03 +0700

    add library

--- server.js
+++ server.js
@@ -2,23 +2,32 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieSession = require('cookie-session');
 const cookieParser = require('cookie-parser')
-
-require('dotenv').config();
-
-const PORT = process.env.PORT | 3030;
 const db = require('./app/database/db_config');
-const app = express();
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const cors = require('cors');
 
 // MODELS 
 const User = require('./app/models/user_models');
 
+const app = express();
+require('dotenv').config();
+
+// PORT
+const PORT = process.env.PORT | 3030;
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cors({ credentials: true }));
+app.use(cookieParser())
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 // SIMPLE API 
 app.get('/', (req, res, next) => {
     res.json({
-        message: "API's Working!"
+        message: "Aksa API is Working!"
     });
 });
 
